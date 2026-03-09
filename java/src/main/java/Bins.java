046b53324409634233adeb18134ebbb36be28fee
nishat <nishatlabiba98@gmail.com>
2026-03-08 20:19:34 -0400

    semi-breakthrough

--- java/src/main/java/Bins.java
+++ java/src/main/java/Bins.java
@@ -1,4 +1,16 @@
 
 public class Bins {
+int[] bins;
+int min;
 
+    public Bins(int min, int max) {
+        this.min = min;
+        bins = new int[max - min + 1];
+    }
+    public Integer getBin(int bin) {
+        return bins[bin - min];
+    }
+    public void incrementBin(int bin) {
+        bins[bin - min]++;
+    }
 }
--- java/src/main/java/Simulation.java
+++ java/src/main/java/Simulation.java
@@ -1,5 +1,31 @@
 public class Simulation {
+    int numberOfDice;
+    int numberOfTosses;
+    Bins bins;
+    Dice dice;
+
+    public Simulation(int numberOfDice, int numberOfTosses) {
+        this.numberOfDice = numberOfDice;
+        this.numberOfTosses = numberOfTosses;
+        bins = new Bins(numberOfDice, numberOfDice * 6);
+        dice = new Dice(numberOfDice);
+    }
 
 
-
+public void runSimulation() {
+    for (int i = 0; i < numberOfTosses; i++) {
+        bins.incrementBin(dice.tossAndSum());
+    }
+  }
+public void printResults() {
+    System.out.println("Simulation of " + numberOfTosses + " tosses of " + numberOfDice + " dice:");
+    for (int i = bins.min; i < bins.bins.length + bins.min; i++) {
+        System.out.println(i + ": " + bins.getBin(i));
+    }
+  }
+public static void main(String[] args) {
+    Simulation simulation = new Simulation(2, 1000);
+    simulation.runSimulation();
+    simulation.printResults();
+}
 }
